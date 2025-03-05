d0174ba5f9bc32c37ddc00733f1bc009142ddab9
Diljot Manj <Diljot.manj@gmail.com>
2025-03-05 12:51:46 -0800

    Created the base for each of the 5 graphs

---
+++ ground_station/backend/graph_plot.py
@@ -0,0 +1,24 @@
+import os
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+class LatitudeLongitudePlot:
+    def __init__(self, latitude_longitude_data):
+        self.latitude_longitude_data = latitude_longitude_data
+
+class PressureAltitudePlot:
+    def __init__(self, pressure_altitude_data):
+        self.pressure_altitude_data = pressure_altitude_data
+
+class AltitudeTimePlot:
+    def __init__(self, altitude_time_data):
+        self.altitude_time_data = altitude_time_data
+
+class TemperatureAltitudePlot:
+    def __init__(self, temperature_altitude_data):
+        self.temperature_altitude_data = temperature_altitude_data
+
+class GyroTimePlot:
+    def __init__(self, gyro_time_data):
+        self.gyro_time_data = gyro_time_data
