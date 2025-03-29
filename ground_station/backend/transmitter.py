205c00043fdcd2330db53a0e3d382f58feee8aee
Abhishek Majumdar <abhishekmajumdar@w87-168-154.wireless.uvic.ca>
2025-03-29 15:27:53 -0700

    added PAYLOAD_TYPE_LIST few changes still to make

--- ground_station/backend/transmitter.py
+++ ground_station/backend/transmitter.py
@@ -1,12 +1,24 @@
-'''Imports'''
-from zlib import crc32
-import json
 import struct
 
-'''Global variables'''
-PAYLOAD_TYPE_LIST = [b"POWER", b"PICTURE", b"RESET"] 
+PAYLOAD_TYPE_LIST = [
+    "0000",  # Ping
+    "0001",  # Nominal
+    "0010",  # Low Power
+    "0011",  # Telemetry
+    "0100",  # Camera-1-End
+    "0101",  # Camera-1-MF
+    "0110",  # Camera-2-End
+    "0111",  # Camera-2-MF
+    "1000",  # Request Retransmission
+    "1001",  # Error - CRC
+    "1010",  # Error - Duplication
+    "1011",  # Error - Low Power
+    "1100",  # Ack. Rec. Camera
+    "1101",  # Ack. Rec. Telemetry
+    "1110",  # Ack. Rec. Status
+    "1111",  # Ack. Rec. Error
+]
 MAX_TRANSMISSION_LIMIT = 5 #dummy value small for testing
-
 
 
 class GroundStationTransmitter():
@@ -21,72 +33,38 @@ class GroundStationTransmitter():
 
     
     def construct_packet(self):
-        # Ideal Payload structure BYTES(All other fields will have default values)
-
-        #Acknowleadgement ACK -> Acknowledged, NACK -> Negative Acknowledgement
-
-        #if command type - Power Command
-        '''
-        {
-        "switch_power_mode" : NOMINAL/LOW/READY,
-        "ackowledgement": ACK/NACK 
-        }
-        '''
-
-        #if command type - Take Picture
-        '''
-        {
-        "take_picture": True,
-        "resolution-set": 720p/480p,
-        "ackowledgement": ACK/NACK
-        }
-        '''
-         #if command type - Reset or Change Transmission Frequency
-        '''
-        {
-        "new_transmission_freq": <new frequency>,
-        "reset_obc/subsystems": none
-        "ackowledgement": ACK/NACK
-        }
-        '''
-        # TODO check if payload_type is in the 3-bit list
         if self.payload_type not in PAYLOAD_TYPE_LIST:
             raise IncorrectPayloadTypeException(f"Invalid payload type: {self.payload_type}")
         
-        payload_length = len(self.payload)
+        #TODO add payload_type to the starting of the packet
+        '''data field and offset are none for now'''
+        #TODO append a sequence number at the end of the recieved payload
 
-        #Compute crc checksum
-        crc_input = self.payload_type + self.payload
-        crc_checksum = self.check_sum(crc_input)
+        # payload_length = len(self.payload)
 
-        # Use struct to pack the fixed fields
-        header = struct.pack(
-            ">BBBH2s2s",  # Big-endian format: preamble, type, length, reserved, src, dst
-            self.preamble,
-            self.payload_type,
-            payload_length,
-            0x00,               # Reserved bits (optional)
-            self.src_address,
-            self.dst_address
-        )
+        # #Compute crc checksum
+        # crc_input = self.payload_type + self.payload
+        # crc_checksum = self.check_sum(crc_input)
+
+        # # Use struct to pack the fixed fields
+        # header = struct.pack(
+        #     ">BBBH2s2s",  # Big-endian format: preamble, type, length, reserved, src, dst
+        #     self.preamble,
+        #     self.payload_type,
+        #     payload_length,
+        #     0x00,               # Reserved bits (optional)
+        #     self.src_address,
+        #     self.dst_address
+        # )
 
         # Pack CRC32 and EOF bit
-        footer = struct.pack(">I B", crc_checksum)
+        # footer = struct.pack(">I B", crc_checksum)
 
-        # Final packet: header + payload + footer
-        packet = header + self.payload + footer
+        # # Final packet: header + payload + footer
+        # packet = header + self.payload + footer
 
         return packet
 
-        
-    def check_sum(self, data: bytes):
-        '''
-        Computes a 32-bit crc checksum for the given stream of bytes
-        :return: A 32-bit integer representing the CRC32 checksum.
-        '''
-
-        return crc32(data)
-    
     def transmit_packet(self, transmit_func):
         '''
         Transit the constructed packet by calling the transmit function
